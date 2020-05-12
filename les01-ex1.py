22a5100c0aa869b79891c3c0c77f6d1b39cfaaf1
pomoal <pomortsev.alex@gmail.com>
2020-05-12 07:39:41 +0700

    Пробую выгрузить отдельными файлами

---
+++ les01-ex1.py
@@ -0,0 +1,7 @@
+# Задание №1
+
+ age = int(input('Введите ваш возраст:\n'))
+ your_height = int(input('Введите ваш рост:\n'))
+ your_name = input('Введите ваше имя:\n')
+
+ print("Вас зовут:",your_name,"\nВам", age,"лет","\nВаш рост:",your_height)
---
+++ les01-ex2.py
@@ -0,0 +1,14 @@
+#Задание №2
+
+import math
+sec_quantity = int(input("Введите число секунд:\n"))
+
+hours_count = math.floor(sec_quantity / 3600);
+min_count = math.floor(math.fmod(sec_quantity,3600) / 60);
+sec_count = sec_quantity - 3600 * hours_count - min_count * 60
+
+if hours_count > 23:
+    days_count = math.floor(hours_count / 24);
+    print(f' {days_count} days : {hours_count} hours : {min_count} mins : {sec_count} secs ')
+else:
+    print(f' {hours_count} hours : {min_count} mins : {sec_count} secs')
